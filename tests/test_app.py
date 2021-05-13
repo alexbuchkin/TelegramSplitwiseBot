dd7e77c634bb165a22f3a6e36c4488899d13ee89
Alex <alexbuchkin96@gmail.com>
2021-05-14 00:00:46 +0300

    test_adding_users_to_events added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import pytest
+import sqlite3
 
 from app.splitwise import SplitwiseApp
 from database.types import (
@@ -26,8 +27,12 @@ def db_name(tmpdir):
     return str(tmpdir / 'db.sqlite')
 
 
-def test_storing_users(db_name):
-    app = SplitwiseApp(db_name)
+@pytest.fixture(scope='function')
+def app(db_name):
+    return SplitwiseApp(db_name)
+
+
+def test_storing_users(app):
     for user in USERS:
         app.add_new_user(user)
 
@@ -44,8 +49,7 @@ def test_storing_users(db_name):
     assert stored_users == USERS
 
 
-def test_storing_events(db_name):
-    app = SplitwiseApp(db_name)
+def test_storing_events(app):
     for user in USERS:
         app.add_new_user(user)
     for user, event in zip(USERS, EVENTS):
@@ -54,8 +58,38 @@ def test_storing_events(db_name):
     for event in EVENTS:
         assert app.get_event_info(event.token) == event
 
-    with pytest.raises(KeyError):
-        app.get_event_info('no_such_token')
+    NONEXISTENT_TOKEN = 'nonexistent_token'
+    with pytest.raises(KeyError, match=f'Event with token {NONEXISTENT_TOKEN} does not exist'):
+        app.get_event_info(NONEXISTENT_TOKEN)
+
+    # trying to create event that already exists
+    with pytest.raises(sqlite3.IntegrityError, match='UNIQUE constraint failed.*'):
+        app.create_event(user_id=USERS[0].id, event_name=EVENTS[0].name, event_token=EVENTS[0].token)
 
     stored_events = sorted(app.get_all_events(), key=lambda event: event.token)
     assert stored_events == EVENTS
+
+
+def test_adding_users_to_events(app):
+    for user in USERS:
+        app.add_new_user(user)
+
+    app.create_event(user_id=USERS[0].id, event_name=EVENTS[0].name, event_token=EVENTS[0].token)
+    assert app.user_participates_in_event(user_id=USERS[0].id, event_token=EVENTS[0].token)
+    for user in USERS[1:]:
+        assert not app.user_participates_in_event(user_id=user.id, event_token=EVENTS[0].token)
+        app.add_user_to_event(user_id=user.id, event_token=EVENTS[0].token)
+        assert app.user_participates_in_event(user_id=user.id, event_token=EVENTS[0].token)
+
+    for user, event in zip(USERS[1:], EVENTS[1:]):
+        app.create_event(user_id=user.id, event_name=event.name, event_token=event.token)
+        for another_user in USERS:
+            if another_user.id != user.id:
+                app.add_user_to_event(user_id=another_user.id, event_token=event.token)
+
+    for user in USERS:
+        for event in EVENTS:
+            assert app.user_participates_in_event(user_id=user.id, event_token=event.token)
+
+    expected_user2event_data = [(user.id, event.token) for user in USERS for event in EVENTS]
+    assert sorted(expected_user2event_data) == sorted(app.get_all_user2event())
